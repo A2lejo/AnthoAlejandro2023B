f706d3301800d38cdb1316275262b0e7422e8a37
Alejandro <alejandro27campoverde@gmail.com>
2023-11-14 09:21:25 -0500

    Update HelloWorld.java
    Segundo cambio

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hola con todos , jasdja");
+
+		System.out.println("Hola con todos, necesito aprender a hacer esto");
 	}
 }
+
